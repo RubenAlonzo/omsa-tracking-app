5e1a06b91e92a7e213ba6e72d3e13b9518dc3135
Ruben <ralonzo@cyberwarrior.com>
2024-04-05 08:01:31 -0400

    Create react project

---
+++ home/home.js
@@ -0,0 +1,25 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru 
+  const position = { lat: 18.477350221873447, lng: -69.91389877233155 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
