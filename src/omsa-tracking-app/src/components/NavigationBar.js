a77f923a53083ba87303d11f14416fed56ff5ce3
Joy2301 <el_jhonny23@hotmail.com>
2024-04-10 02:42:49 -0400

    add-cosillas

--- src/omsa-tracking-app/src/components/NavigationBar.js
+++ src/omsa-tracking-app/src/components/NavigationBar.js
@@ -1,7 +1,24 @@
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { FaHeart, FaHome, FaInfoCircle } from 'react-icons/fa';
+import HeartSelected from "../components/icons/heart-selected";
+import HeartUnselected from "../components/icons/heart=unselected";
+import ListSelected from "../components/icons/navigation/list-selected";
+import ListUnSelected from "../components/icons/navigation/list-unselected";
+import LocationSelected from "../components/icons/navigation/location-selected";
+import LocationUnSelected from "../components/icons/navigation/location-unselected";
 
 // TODO: Change icons according to the design. Use filled icons when page is active
 function NavigationBar() {
+
+  const location = useLocation();
+  const [selectedPage, setSelectedPage] = useState('');
+
+  useEffect(() => {
+    // Actualiza el estado de la página seleccionada cuando cambia la ubicación
+    setSelectedPage(location.pathname);
+  }, [location]);
+
   return (
     <nav className="max-w-[480px] bg-white flex justify-between items-center h-16 px-0 pb-1 mt-3 w-full fixed bottom-0 w-full z-10 border-t-indigo-500" style={{ boxShadow: '2px 0px 8px -2px gray' }}>
       <ul className="flex justify-evenly w-full">
@@ -9,59 +26,30 @@ function NavigationBar() {
           <Link
             to="/favorites"
             className="flex items-center text-gray-700 hover:text-blue-500"
+            onClick={() => setSelectedPage('/favorites')}
+            style={{ cursor: 'pointer' }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-10 h-10"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-              />
-            </svg>
+            {selectedPage === '/favorites' ? <HeartSelected /> : <HeartUnselected />}
           </Link>
         </li>
         <li className="flex items-center w-[160px] justify-center">
           <Link
             to="/home"
             className="flex items-center text-gray-700 hover:text-blue-500"
+            onClick={() => setSelectedPage('/home')}
+            style={{ cursor: 'pointer' }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-10 h-10"
-            >
-              <path
-                fillRule="evenodd"
-                d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            {selectedPage === '/home' ? <LocationSelected /> : <LocationUnSelected />}
           </Link>
         </li>
         <li className="flex items-center w-[160px] justify-center">
           <Link
             to="/details"
             className="flex items-center text-gray-700 hover:text-blue-500"
+            onClick={() => setSelectedPage('/details')}
+            style={{ cursor: 'pointer' }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-10 h-10"
-            >
-              <path
-                fillRule="evenodd"
-                d="M2.625 6.75a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0A.75.75 0 0 1 8.25 6h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75ZM2.625 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0ZM7.5 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12A.75.75 0 0 1 7.5 12Zm-4.875 5.25a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875 0a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5h-12a.75.75 0 0 1-.75-.75Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            {selectedPage === '/details' ? <ListSelected /> : <ListUnSelected />}
           </Link>
         </li>
       </ul>
